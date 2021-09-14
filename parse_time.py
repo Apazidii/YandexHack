056673dccc36ff74a854a19b6898fa831cfd7cd1
Ivchenko Nikolay <mrsheldie@mail.ru>
2021-09-14 18:24:36 +0300

    Import bs4

--- parse_time.py
+++ parse_time.py
@@ -1,18 +1,19 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-html = urlopen("https://mostotrest-spb.ru").read().decode('utf-8')
+def parse_bridges():
+    html = urlopen("https://mostotrest-spb.ru").read().decode('utf-8')
 
-soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
-bridges = []
-bridge = soup.find('div', 'bridge')
-bridges.append(bridge)
+    bridges = []
+    bridge = soup.find('div', 'bridge')
+    bridges.append(bridge)
 
-while bridge:
-    bridges.append(bridge.find_next_sibling('div', 'bridge'))
-    bridge = bridge.find_next_sibling('div', 'bridge')
+    while bridge:
+        bridges.append(bridge.find_next_sibling('div', 'bridge'))
+        bridge = bridge.find_next_sibling('div', 'bridge')
 
-#print(bridges[0])
+    return bridges
 
 #print(bridge.find_next_sibling('div', 'bridge'))
