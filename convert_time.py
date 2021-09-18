06615ef92924069654707714f329d5ada27cb1dc
kosty <kostya.apazidi@gmail.com>
2021-09-18 14:58:17 +0300

    Небольшие доработки

--- convert_time.py
+++ convert_time.py
@@ -15,33 +15,32 @@ def get_ending(num: int) -> str:
     elif num % 10 in {1}:
         return ""
 
-def time_to_text(now, most):
+def time_to_text(now, most, ra):
     res = ""
 
-    t_start = datetime.timedelta(hours = int(most[0][:1]), minutes = int(most[0][2:]))
-    t_stop  = datetime.timedelta(hours = int(most[1][:1]), minutes = int(most[1][2:]))
+    move = ["разведен", "сведен"]
+    t = [datetime.timedelta(hours = int(most[0][:1]), minutes = int(most[0][2:])) ,datetime.timedelta(hours = int(most[1][:1]), minutes = int(most[1][2:]))]
     now     = datetime.timedelta(hours = int(now[:1])    , minutes = int(now[2:]))
-    c = t_start - now
+    c = t[ra] - now
     tm = int(c.seconds / 60)
-    print("Минут:",tm)
     if tm == 30:
-        res = "Мост будет разведен ровно через полчаса, в " + most[0]
+        res = "Мост будет " +move[ra]+ " ровно через полчаса, в " + most[0]
     elif tm < 30 and tm >=28:
-        res = "Мост будет разведен чуть меньше чем через полчаса, в " + most[0]
+        res = "Мост будет " +move[ra]+ " чуть меньше чем через полчаса, в " + most[0]
     elif tm > 30 and tm <=32:
-        res = "Мост будет разведен чуть больше чем через полчаса, в " + most[0]
+        res = "Мост будет " +move[ra]+ " чуть больше чем через полчаса, в " + most[0]
     elif tm == 60:
-        res = "Мост будет разведен ровно через час, в " + most[0]
+        res = "Мост будет " +move[ra]+ " ровно через час, в " + most[0]
     elif tm > 60 and tm <= 65:
-        res = "Мост будет разведен чуть больше чем через час, в " + most[0]
+        res = "Мост будет " +move[ra]+ " чуть больше чем через час, в " + most[0]
     elif tm < 60 and tm >= 55:
-        res = "Мост будет разведен чуть меньше чем через час, в " + most[0]
+        res = "Мост будет " +move[ra]+ " чуть меньше чем через час, в " + most[0]
     elif tm == 90:
-        res = "Мост будет разведен ровно через полторачаса, в " + most[0]
+        res = "Мост будет " +move[ra]+ " ровно через полторачаса, в " + most[0]
     elif tm < 90 and tm >= 80:
-        res = "Мост будет разведен чуть меньше чем через полторачаса, в " + most[0]
+        res = "Мост будет " +move[ra]+ " чуть меньше чем через полторачаса, в " + most[0]
     elif tm > 90 and tm <= 100:
-        res = "Мост будет разведен чуть больше чем через полторачаса, в " + most[0]
+        res = "Мост будет " +move[ra]+ " чуть больше чем через полторачаса, в " + most[0]
     else:
         if tm > 60 and tm < 120:
             ok = 5
@@ -54,11 +53,11 @@ def time_to_text(now, most):
 
 
         if ntm == tm:
-            res = "Мост будет разведен через "
+            res = "Мост будет " +move[ra]+ " через "
         elif ntm < tm:
-            res = "Мост будет разведен чуть меньше чем через "
+            res = "Мост будет " +move[ra]+ " чуть меньше чем через "
         elif ntm > tm:
-            res = "Мост будет разведен чуть больше чем через "
+            res = "Мост будет " +move[ra]+ " чуть больше чем через "
 
         m = int(ntm % 60)
         h = int(ntm / 60)
@@ -79,114 +78,115 @@ def time_to_text(now, most):
         res = res + ", в " + most[0]
     return (res)
 
+print(time_to_text(now, most, 1))
 
-def prints():
-    global now
-    global most
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
 
-    most[0] = "0:06"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
+# def prints():
+#     global now
+#     global most
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "0:06"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "0:12"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "0:28"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "0:30"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "0:31"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "0:47"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "0:55"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "1:00"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "1:02"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "1:15"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "1:16"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "1:20"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "1:30"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "1:38"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "2:00"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "2:02"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
+#
+#     most[0] = "4:00"
+#     now     = "0:00"
+#     print("Время:",now,"Время разводки моста:",most[0])
+#     print("Фраза Aлисы:", time_to_text(now, most))
+#     print("----------------------------------")
 
-    most[0] = "0:12"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "0:28"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "0:30"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "0:31"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "0:47"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "0:55"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "1:00"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "1:02"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "1:15"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "1:16"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "1:20"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "1:30"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "1:38"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "2:00"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "2:02"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-    most[0] = "4:00"
-    now     = "0:00"
-    print("Время:",now,"Время разводки моста:",most[0])
-    print("Фраза Aлисы:", time_to_text(now, most))
-    print("----------------------------------")
-
-prints()
